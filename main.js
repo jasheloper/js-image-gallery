2c7c4886fbd9941dee86a6241813e9dba2f88dbd
jasheloper <48870048+jasheloper@users.noreply.github.com>
2024-01-02 16:23:03 -0700

    bottom thumbnails are showing

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const fileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const imgFileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+console.log(`The length of the imgFileNames array is: ${imgFileNames.length}`);
 
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < imgFileNames.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${imgFileNames[i]}`);
+    newImage.setAttribute('alt', imgFileNames[i]);
+    thumbBar.appendChild(newImage);
+}
+
 
 /* Wiring up the Darken/Lighten button */
