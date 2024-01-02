2907876dfe8224a24594a68c68a53719e30e3519
jasheloper <48870048+jasheloper@users.noreply.github.com>
2024-01-02 15:55:29 -0700

    replaced with my own images also added filenames array

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const fileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 
