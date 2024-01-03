e907cd68b355b28cd0ac0c2eac78e7708be00dcd
jasheloper <48870048+jasheloper@users.noreply.github.com>
2024-01-02 17:06:47 -0700

    read me image

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const imgFileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const imgAltNames = ["Valley of the Sun", "Mountain Walk", "Desert Flowers", "Mountains and Desert", "Sunrise"];
 
 /* Declaring the alternative text for each image file */
 
@@ -14,7 +15,7 @@ const imgFileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"
 for (let i = 0; i < imgFileNames.length; i++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/${imgFileNames[i]}`);
-    newImage.setAttribute('alt', imgFileNames[i]);
+    newImage.setAttribute('alt', imgAltNames[i]);
     thumbBar.appendChild(newImage);
 
     newImage.addEventListener("click", (event) => {
